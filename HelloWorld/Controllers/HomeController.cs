b52bbaf3f83a69c9a92869827c68e8ea78868270
Maciek <maciek.d@me.com>
2018-09-22 02:02:33 -0700

    Initial commit

---
+++ HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HelloWorld.Models;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Message = DateTime.Now.ToShortDateString();
+            var address = new Adress
+            {
+                City = "Warsaw",
+                StreetName = "Kopernika",
+                Building = "30"
+            };
+            return View(address);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HelloWorld/Models/Adress.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Models
+{
+    public class Adress
+    {
+        public string City { get; set; }
+        public string StreetName { get; set; }
+        public string Building { get; set; }
+    }
+}
